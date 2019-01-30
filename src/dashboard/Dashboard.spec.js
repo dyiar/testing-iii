03da9de402cfcd723e6adee9f18ca8ff8c1fb26d
dyiar <austin.hl.blake@gmail.com>
2019-01-30 14:25:42 -0700

    added test to dashboard to see if controls and display are displaying

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+    it('should render without crashing', () => {
+        const { getByTestId } = render(<Dashboard />);
+
+        getByTestId('locked')
+        getByTestId('closed')
+        getByTestId('locked-button')
+        getByTestId('closed-button')
+    })
+})
