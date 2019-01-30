532eb503b6761698071c0466e854dc22d52fc3c5
dyiar <austin.hl.blake@gmail.com>
2019-01-30 14:07:55 -0700

    initial commit. made tests for display.

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+import Display from './Display';
+
+describe('<Display />', () => {
+    it('should render without crashing', () => {
+        const { getByTestId } = render(<Display />);
+
+        getByTestId('locked')
+        getByTestId('closed')
+    })
+})
